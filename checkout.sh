d5ff8b92eb1f611539b9700401a6ab1a957f9a59
Marcus1911 <marcus.sandri@ufscar.br>
2014-09-01 00:46:41 -0300

    introduced checkout script

---
+++ checkout.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+echo "\n Welcome to Multiflow Script. The following demo will reproduce all graphs and testbeds \n"
+echo "Searching fundamental library..."
+sudo apt-get install scapy git git-all
+
+
+
