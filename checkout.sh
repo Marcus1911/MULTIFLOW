11b688d05c309133b92095a657777acc788b7886
Marcus1911 <marcus.sandri@ufscar.br>
2014-10-27 17:46:42 -0200

    Update checkout essentials

--- checkout.sh
+++ checkout.sh
@@ -2,7 +2,7 @@
 
 echo "\n Welcome to Multiflow Script. The following demo will reproduce all graphs and testbeds \n"
 echo "Searching fundamental library..."
-sudo apt-get install scapy git git-all
+sudo apt-get install python-numpy pypy python-matplotlib ipython python-networkx 
 
 
 
