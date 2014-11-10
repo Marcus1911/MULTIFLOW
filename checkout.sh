8befea1b4fe67685f20b35b347cbb1ba42618947
Marcus1911 <marcus.sandri@ufscar.br>
2014-11-10 13:56:46 -0200

    Multiflow essentials

--- checkout.sh
+++ checkout.sh
@@ -2,7 +2,12 @@
 
 echo "\n Welcome to Multiflow Script. The following demo will reproduce all graphs and testbeds \n"
 echo "Searching fundamental library..."
-sudo apt-get install python-numpy pypy python-matplotlib ipython python-networkx mn 
+
+sudo apt-get install python-numpy pypy python-matplotlib ipython python-networkx  
+
+
+
+
 
 
 
