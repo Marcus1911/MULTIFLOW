65053b27a92bf67e21e5fa549e590ee23d124ed4
Marcus1911 <marcus.sandri@ufscar.br>
2014-11-11 20:43:42 -0200

    POX installation/Mininet from apt/Simulation triggers

--- checkout.sh
+++ checkout.sh
@@ -3,8 +3,15 @@
 echo "\n Welcome to Multiflow Script. The following demo will reproduce all graphs and testbeds \n"
 echo "Searching fundamental library..."
 
-sudo apt-get install python-numpy pypy python-matplotlib ipython python-networkx  
+sudo apt-get update
+sudo apt-get install python-numpy pypy python-matplotlib ipython python-networkx python-setuptools
 
+cd ~/
+git clone git://github.com/noxrepo/pox.git
+cd pox
+git checkout master
+cp ~/MULTIFLOW/Multiflow-Discrete-Time-1.py ../pox/ext/
+cp ~/MULTIFLOW/Multiflow-Discrete-Time-2.py ../pox/ext/
 
 
 
