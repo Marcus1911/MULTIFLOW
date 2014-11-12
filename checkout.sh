a0a625d0c171f48ce10cd91cbc26e790cd4f355a
Marcus1911 <marcus.sandri@ufscar.br>
2014-11-11 23:20:16 -0200

    POX adjusted to dart-version

--- checkout.sh
+++ checkout.sh
@@ -4,12 +4,12 @@ echo "\n Welcome to Multiflow Script. The following demo will reproduce all grap
 echo "Searching fundamental library..."
 
 sudo apt-get update
-sudo apt-get install python-numpy pypy python-matplotlib ipython python-networkx python-setuptools
+sudo apt-get install python-numpy pypy python-matplotlib ipython python-networkx python-setuptools mininet
 
 cd ~/
 git clone git://github.com/noxrepo/pox.git
 cd pox
-git checkout master
+git checkout dart
 cp ~/MULTIFLOW/Multiflow-Discrete-Time-1.py ../pox/ext/
 cp ~/MULTIFLOW/Multiflow-Discrete-Time-2.py ../pox/ext/
 
