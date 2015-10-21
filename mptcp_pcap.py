090d94d3e14700020c33855d23acc61410589374
Marcus Sandri <marcus.sandri@ufscar.br>
2015-10-21 00:30:02 -0200

    fixed bugs in packet processing

--- mptcp_pcap.py
+++ mptcp_pcap.py
@@ -4,9 +4,9 @@ import pox.lib.pxpcap.parser as pxparse
 import networkx as nx
 import time
 
-startTime = time.time()
 G=nx.path_graph(100)
 
+log = core.getLogger()
 
 counter = 0
 def beUnpack(byte):
@@ -17,11 +17,10 @@ def beUnpack(byte):
          return 'impossible to convert'
 
 def Multiflow (data, parser):
-
-  global counter
-  counter += 1
+    
   packet = pkt.ethernet(data)
   packet_tcp = packet.find('tcp')
+
   if not packet_tcp: return
   if packet_tcp.SYN and packet_tcp.ACK: return
   if packet_tcp.SYN is True:
@@ -30,7 +29,8 @@ def Multiflow (data, parser):
       if isinstance(option, pkt.TCP.mptcp_opt):
         name_option = type(option).__name__ #,vars(option)
         if name_option == 'mp_capable_opt':
-          print "MP_CAPABLE\n"
+            #changed to log.info()  
+          log.info("MP_CAPABLE\n")           
         if name_option == 'mp_join_opt':
           hash_key = beUnpack(option.rtoken)
           Hash_table = dict()
@@ -38,22 +38,47 @@ def Multiflow (data, parser):
           path = nx.single_source_shortest_path(G,0)
           lista.append([1,2,3])
           topologia = str(lista)  
-          Hash_table[hash_key] = topologia # Append token and topology 
+          #Hash_table[hash_key] = topologia # Append token and topology 
+          #FIXME: Associate the hash in the constructor
+
 	  resultado = Hash_table.get(hash_key)
-          print "MP_JOIN", hash_key
+          #keeped with print, due log.info errors
+          print "MP_JOIN", resultado 
          
-          if resultado is None:
-           # print "There's no Token. Multiflow will add for you..."
+          if resultado is None:  
+            #Hash_table[hash_key] = topologia
+            log.info("Matrix is empty. Populating...")
+            #...Gives the shortest-path in the topology  
             path = nx.single_source_shortest_path(G,0)
+            
+            #Add pruned topology and associate it with the token 
             Hash_table[hash_key] = topologia
+           
+            
 
           else:
-               #print "Path:", Hash_table[hash_key] 
-               path = nx.single_source_shortest_path(G,0)
+              
+               try:
+                 print "There's a token"
+                 NewTopology = Hash_table.get(hash_key)
+                 #.. Gives the shortest-path in the topology
+                 path = nx.single_source_shortest_path(G,0)
+
+                 #Add pruned topology and associate it with the token
+                 Hash_table[hash_key] = NewTopology
+
+               except:
+                 #TODO: TEST IT! 
+
+                 print "There's no more shortest-paths"
+
+                 # OF rules for dropping
+
+
                                     
   return
-  elapsedTime = time.time() - startTime
-  print elapsedTime
+
+
 def launch (filename):
   data = open(filename, "r").read()
   p = pxparse.PCapParser(callback=Multiflow)
