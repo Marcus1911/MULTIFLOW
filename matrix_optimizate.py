c4dd69e59e3521971e82b1e87549fcca201459e5
Marcus Sandri <marcus.sandri@ufscar.br>
2015-10-27 23:43:47 -0200

    start to develop virtual-topology

--- matrix_optimizate.py
+++ matrix_optimizate.py
@@ -7,102 +7,146 @@ Este codigo monta a topologia fora do Handle_PacketIn() e
 v. Beta 1.0
 
 """
-import matplotlib.pyplot as plt
-from collections import *
+#import matplotlib.pyplot as plt
+#from collections import *
 import networkx as nx
-import numpy as np
-import matplotlib.pyplot as plt
-import pylab
-import itertools
-import hashlib as hash
+#import numpy as np
+#import matplotlib.pyplot as plt
+#import pylab
+#import itertools
+#import hashlib as hash
 
 #log = core.getLogger()
 
 G = nx.DiGraph() # Inicia G
-switches = {}
 
-class BuildTopo(object):
-  """ Auxiliar e usada para guardar os metodos a serem utilizados. """
+switch_list = list()
+switch_list = [[4, 1, 2, 2], [4, 2, 3, 2], [3, 1, 1, 3], [3, 2, 4, 2], [1, 3, 3, 1], [1, 2, 2, 1], [2, 1, 1, 2], [2, 2, 4, 1], [1, 3, 3, 1], [2, 1, 1, 2], [1, 2, 2, 1], [3, 1, 1, 3], [2, 2, 4, 1], [4, 1, 2, 2], [4, 2, 3, 2], [3, 2, 4, 2]]
 
 
-  def __init__ (self):
-    self.test = None
-	
-  def uniq(self, input):
-  # Funcao retira valores repetidos da lista.
-  # Entra com lista inteira e retorna lista sem 
-  # valores repetidos
 
+#vertex = [1,2,3,4,5]
+#edges = [[1, 2], [2, 1], [1, 4],[4, 1], [1, 3],[3, 1], [4, 3], [2, 3], [2, 3], [4, 2], [5, 3], [3, 5], [4, 5], [5, 4]]
+
+def rem_repeated_links(input):
+    #TODO: Remove XXXX and add the Class name
+    """
+    When XXXX listen 'openflow-dicovery' module, it
+    listen also repeated values. This function remove 
+    those values.
+    """
     output = []
     for x in input:
       if x not in output:
         output.append(x)
     return output
 	
+def remove_value(search, input):
+  """
+  This function is used to remove values in a list. 
+  We use it @ 'rem_repeated_links' 
+  """
+    
+  output = []
+  for x in input:
+    output.append(x)
+    if search in output:
+      output.remove(x)
+  return output
 
-  def del_val(self, search, input):
-  # Funcao deleta valores da lista
-  # del_val(valor_a_ser_deletado, lista)
-
-    output = []
-    for x in input:
-      output.append(x)
-      if search in output:
-        output.remove(x)
-    return output
-
-
-
-  def new_topology(self, topo, sws):
+def all_disjointed_paths(vertex, edge, src, dst):
     """
-    Funcao retorna nova topologia 
-    new_topology(topologia, switch a ser removido) 
+    This function returns all_di(sjointed_paths
+    Use: all_disjointed_paths(vertx,edge,source,destination)
+    Will return all shortest and disointed paths between
     """
-    newlist = []
-    i = 0 
-    for i in range(len(topo)):
-      if topo[i][0] != sws:     
-        newlist.append(topo[i])
-    return newlist
-
-
-  def grafo(self, vertice, aresta, src, dst):
-  # Funcao retorna menor caminho
-  # Entra com: grafo(vertice,aresta,origem,destino)
-  # Ira retornar o menor caminho calculado por Dijkstra
-
+    all_paths = list()
     G.clear()
-    G.add_nodes_from(vertice) # colocar vertice no lugar de new_list
-    G.add_edges_from(aresta) # colocar aresta no lugar de edge
-    return nx.dijkstra_path(G,src,dst)
+    G.add_nodes_from(vertex) # colocar vertice no lugar de new_list
+    G.add_edges_from(edge) # colocar aresta no lugar de edge
+    [[all_paths.append(i)] for i in nx.all_shortest_paths(G,src,dst)] # pretty! 
+    return all_paths
+
+
+
+
+def filter_edge_vertex(switch_list):
+  #TODO: Translate the function to be used with constructor
+  """ 
+  This function is used for desassembly
+  a graph G(V,E). Then, it will be used in
+  all_disjointed_paths()
+  
+  Use: filter_edge_vertex(list of switches)
+  Will return: edge and vertex
+  """
+  
+  temporary_edge = list()
+  temporary_vertex = list()
+
+  for i in switch_list:
+        temporary_edge.append((i[0], i[2]))
+  edge = rem_repeated_links(temporary_edge) 
+
+  for i in switch_list:
+        temporary_vertex.append(i[0])
+  vertex = rem_repeated_links(temporary_vertex) 
+  return edge, vertex
+
+
+def swport_to_node(paths, switch_list):
+  """
+  This function is used for joint all 
+  switchports with some node
+  
+  Use: swport_to_node(given path, list of
+  switches)
+  """
+  def bucket(input, value):
+    return filter(lambda x: x[0] == value, input)
+  #TODO: USE LIST COMPREHENSION  
+  sw = rem_repeated_links(switch_list)
+  node = dict()
+  for i in paths:
+    fil = bucket(sw, i)
+    node[i] = fil
+  return node
+
+
+
+
+
+
+
+# Filtered values
+filtered_sw = filter_edge_vertex(switch_list) 
+edges = filtered_sw[0]
+vertex = filtered_sw[1]
+
+# Shortest paths 
+shortest_paths = all_disjointed_paths(vertex, edges, 1, 4)
+#print "Shortest-paths", shortest_paths 
+
+
+# JOIN NODES WITH SWITCHPORT  
+paths = shortest_paths[0]
+#[swport_to_node(i, switch_list) for i in shortest_paths]
+virtual_topology = list()
+for i in shortest_paths:
+  virtual_topology.append(swport_to_node(i, switch_list))
+
+
+# Parsing rules
+
+#XXX: Its working fine!
+
+for j in virtual_topology:
+  keys = j.keys()
+  print "\n"
+  for i in keys:
+    raw = j.get(i)
+    for k in raw:
+      print k
+      for g in k:
+        print g # for i in shortest_paths:
  
-
-
-build = BuildTopo()
-switch_list = list()
-switch_list = [[4,3,3,2],[2,3,3,4],[2,3,3,1],[4,2,2,1]]
-
-matriz = build.uniq(switch_list) # matriz sem os valores repetidos
-vertex = list()
-edges = list()
-
-for i in matriz:
-  vertex.append(i[0])
-  edges.append(build.uniq((i[0],i[3])))
-
-print "vertex", build.uniq(vertex)
-print "edge", edges
-
-print "shortest-path", build.grafo(build.uniq(vertex), edges, 2, 4)
-
-switch_removed = build.grafo(build.uniq(vertex), edges, 2, 4)
-
-###### Working on this function #######
-
-for i in switch_removed:
-  nova_topologia = build.new_topology(switch_list, i)
-print nova_topologia
-
-
-
-
